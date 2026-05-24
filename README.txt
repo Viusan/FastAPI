648e13c77f6d99a265728f110f3cf66661ee672c
viusa <viusansantos@gmail.com>
2026-05-24 23:52:31 +0200

    added a user table and more info to read me

--- README.txt
+++ README.txt
@@ -1,5 +1,7 @@
 This is a CRUD API created from scratch using Python, FastAPI and Pydantic
 
 Project is a simple to do list i created just to get used to how FastAPI works
+I am also currently adding a database using SQLite to see how data gets stored and how to add a db to this 
+Later on will try to connect to AWS
 
 To run clone repo, create virtual environment, install dependencies and run the server
--- models.py
+++ models.py
@@ -1,5 +1,13 @@
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
 from database import Base
+
+class User(Base):
+    __tablename__ = "user"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, nullable=False)
+    email = Column(String, nullable=False)
+    password = Column(String, nullable=False)
 
 class Task(Base):
     __tablename__ = "tasks" #tables name in db
@@ -7,6 +15,7 @@ class Task(Base):
     #primary key being true now auto increments id and creates unique
     #nullable being false means it cant be empty
     id = Column(Integer, primary_key=True, index=True) 
+    user_id = Column(Integer, ForeignKey("user.id"), nullable=False) #user.id refferes to id collumn in user table, thats how it links to the table
     name = Column(String, nullable=False)
     description = Column(String, nullable=False)
     done = Column(Boolean, default=False)
