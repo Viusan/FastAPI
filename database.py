c60f79da750169abcf9044dce7d2300ba9daa2fe
viusa <viusansantos@gmail.com>
2026-05-28 13:41:59 +0200

    added docker compose and postgreSQL support

--- database.py
+++ database.py
@@ -1,10 +1,27 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./task.db" #this tells where db files live and "./" creates a task.db in project folder
+#this line basically says if DATABASE_URL env variable exists inside docker, use postgresql
+#if it does not exist then we use sqlitepy 
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./tasks.db")
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}) #engine that connect to db, dont need to worry about the code as much here
+def create_engine_with_retry():
+    retries = 5
+    while retries > 0:
+        try:
+            engine = create_engine(SQLALCHEMY_DATABASE_URL)
+            engine.connect()
+            return engine
+        except Exception:
+            retries -= 1
+            print(f"Database not ready, retrying... {retries} attempts left")
+            time.sleep(3)
+    raise Exception("Could not connect to database")
+
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL) 
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine) #a factory to create sessions
 
